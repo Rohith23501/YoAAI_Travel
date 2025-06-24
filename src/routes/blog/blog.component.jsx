c71728f08f23493662beca611b1db450b2ba9373
rohit <rohithrvp99@gmail.com>
2025-06-24 09:45:10 +0530

    see if this works this time

---
+++ src/routes/blog/blog.component.jsx
@@ -0,0 +1,14 @@
+
+import BlogDirectory from '../../components/blog_directory/blog_directory.component.jsx';
+import './blog.styles.scss';
+const Blog = () => {
+    return (
+        <div className="blog-container">
+           
+            <BlogDirectory />
+            
+        </div>
+    );
+}
+
+export default Blog;
---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,35 @@
+import {Outlet, Link} from "react-router-dom";
+
+
+import './navigation.styles.scss';
+
+const Navigation = () => 
+{
+    return (
+        <div>
+        <header className="navigation-header">
+        
+            <div className="navigation">
+                <Link className="logo-container" to="/">
+                     <img src="https://i.imgur.com/ABY36uM.jpeg" alt="Logo" className="logo" />    
+                     <span> Your Adventure AI</span> {/*your travelling companion */}                  
+                </Link>
+                <div className="nav-links-container">
+                    <Link className="nav-link" to="/">Home</Link>
+                    <Link className="nav-link" to="/blog">blog</Link>
+                    <Link className="nav-link" to = "/contact">contact us</Link>
+                    <Link className="nav-link" to = "/about-us">about us</Link>
+                    <Link className="nav-link" to = "/faq">Faq</Link>
+                    <Link className="button-element" to = "/sign-in">sign up</Link>
+                </div>
+            </div>
+            
+     
+        
+        </header>
+        <Outlet/>
+        </div>
+    )
+}
+
+export default Navigation;
